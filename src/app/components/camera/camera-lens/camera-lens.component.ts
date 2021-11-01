af13e544043c820299d4aa64fb5ed841211ce92f
barriosx <stevenbarrios03@gmail.com>
2021-10-31 22:31:34 -0400

    Inject camera service into camera-lens component and produce feed from camera

--- src/app/components/camera/camera-lens/camera-lens.component.ts
+++ src/app/components/camera/camera-lens/camera-lens.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CameraService } from 'src/app/services/camera/camera.service';
 
 @Component({
   selector: 'app-camera-lens',
   templateUrl: './camera-lens.component.html',
 })
 export class CameraLensComponent implements OnInit {
+  @ViewChild('video', { static: true }) video!: ElementRef;
+  cameraStream: MediaStream | null = null;
+  cameraHeight = 0;
+  cameraWidth = 0;
 
-  constructor() { }
+  constructor(private cameraService: CameraService) { }
 
   ngOnInit(): void {
+    this.cameraService.getCameraFeed().then(stream => {
+      console.log(stream);
+      this.cameraStream = stream;
+      this.video.nativeElement.srcObject = stream;
+    })
+    .catch(err => {
+      console.error(err);
+    });
   }
-
+  onCameraMetadataLoaded(e: any) {
+    console.log(e);
+    this.cameraHeight = e.target.videoHeight;
+    this.cameraWidth = e.target.videoWidth;
+  }
 }
