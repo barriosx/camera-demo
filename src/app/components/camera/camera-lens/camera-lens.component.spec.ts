493bf495553c9218d8b7b9c74c0195034ca04123
barriosx <stevenbarrios03@gmail.com>
2021-11-10 19:11:36 -0500

    Refactor camera lens component to handle switching off the camera experience and show camera roll instead

--- src/app/components/camera/camera-lens/camera-lens.component.spec.ts
+++ src/app/components/camera/camera-lens/camera-lens.component.spec.ts
@@ -1,19 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CameraService } from 'src/app/services/camera/camera.service';
 
 import { CameraLensComponent } from './camera-lens.component';
 
 describe('CameraLensComponent', () => {
   let component: CameraLensComponent;
   let fixture: ComponentFixture<CameraLensComponent>;
-
+  let service: CameraService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CameraLensComponent ]
+      declarations: [ CameraLensComponent ],
+      imports: [
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    service = TestBed.inject(CameraService);
     fixture = TestBed.createComponent(CameraLensComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,4 +33,13 @@ describe('CameraLensComponent', () => {
     expect(component.cameraHeight).toBe(100);
     expect(component.cameraWidth).toBe(100);
   });
+  it('should not get camera feed if viewing camera roll', () => {
+    service.changeView();
+    expect(component.showingCamera).toBeFalse();
+  });
+  it('should call function to show camera roll', fakeAsync(() => {
+    component.goToRoll();
+    tick();
+    expect(component.showingCamera).toBeFalse();
+  }));
 });
