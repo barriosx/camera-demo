0dc8fe2f4963c94d20e2c70c29ca547204c5fcd6
barriosx <stevenbarrios03@gmail.com>
2021-11-10 19:12:06 -0500

    Add camera lens animations

---
+++ src/app/constants/animations.ts
@@ -0,0 +1,32 @@
+import { trigger, transition, style, animate, state, query, animateChild, group } from '@angular/animations';
+
+export const cameraAnimations = [
+  trigger('photoSaved', [
+    transition(':increment', [
+      style({ opacity: 0, transform: 'scale(0)' }), animate('150ms', style({opacity: 1, transform: 'scale(1)'}))
+    ]),
+    transition(':enter', [
+      style({ opacity: 0, transform: 'scale(0)' }), animate('200ms', style({opacity: 1, transform: 'scale(1)'}))
+    ])
+  ]),
+  trigger('lens', [
+    state('active', style({
+      background: 'transparent'
+    })),
+    state('idle', 
+    style({
+      background: 'black'
+    })),
+    transition('active <=> idle', [
+      group([
+        query('@feed', animateChild()),
+      ])
+    ])
+  ]),
+  trigger('feed', [
+    state('idle', style({ filter: 'blur(5px)' })),
+    transition('* <=> *', [
+      animate('500ms 600ms cubic-bezier(0.16,1,0.3,1)')
+    ])
+  ])
+]
