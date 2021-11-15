357913f1520c2b3bef70d4b0b85423af0f031e3a
barriosx <stevenbarrios03@gmail.com>
2021-11-14 20:52:54 -0500

    implement alert add method

--- src/app/services/alert/alert.service.spec.ts
+++ src/app/services/alert/alert.service.spec.ts
@@ -1,4 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { filter } from 'rxjs/operators';
+import { Alert } from 'src/app/constants/alert';
 
 import { AlertService } from './alert.service';
 
@@ -12,5 +14,15 @@ describe('AlertService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should be able to add alert', done => {
+      service.alert$.pipe(filter(alert => !!alert)).subscribe(alert => {
+        console.log(alert);
+        
+        expect(alert).toBeDefined();
+        expect(alert?.title).toEqual('Error');
+        done();
+      });
+      service.addAlert({name: 'Error', message: 'Hello world!'});
   });
 });
